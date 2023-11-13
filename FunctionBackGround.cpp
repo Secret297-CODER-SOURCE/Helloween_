4d190f50149a1ef0edaa6159a8f3d2213cdabe15
Kyrsanov Kyrylo <k.kirill.k.08@gmail.com>
2023-11-13 21:15:54 +0200

    New Function

--- FunctionBackGround.cpp
+++ FunctionBackGround.cpp
@@ -8,19 +8,30 @@
 #include <codecvt>
 using namespace std;
 
-//test comment
-
 void SearchFileInDirectory(const std::string& targetFileName, const std::string& searchDirectory) {
     try {
         for (const auto& entry : filesystem::directory_iterator(searchDirectory)) {
             if (entry.is_regular_file()) {
                 std::string filename = entry.path().filename().string();
-                if (filename == targetFileName) {
+                if (filename == "qwerty.png") {
+                    cout << "qwer";
+                }
+
+                string format;
+                format.resize(4);
+                int j = 0;
+                int i = filename.size() - 4;
+                for (; j < 4; i++, j++) {
+                    format[j] = filename[i];
+                }
+
+                if (format == ".png") {
                     // Файл с искомым именем найден
                     std::wcout << L"Файл найден: " << entry.path() << std::endl;
 
                     // Установить фотографию в качестве обоев
                     SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (void*)entry.path().c_str(), SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+                    system("pause");
                     return; // Если файл найден, завершаем поиск
                 }
             }
@@ -38,7 +49,7 @@ void SearchFileInDirectory(const std::string& targetFileName, const std::string&
 int main()
 {
     setlocale(LC_ALL, "rus");
-    string targetFileName = "hq720.jpg";
+    string targetFileName = "qwerty.png";
 
     //// Начнем поиск с корневого каталога системного диска (обычно C:\)
     string searchDirectory = "C:\\Users\\Вадим\\Desktop";
