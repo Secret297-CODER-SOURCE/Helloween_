68befff2200d95aee5fe6395ed818c26610bc670
Gabby <105376275+Gabbiston@users.noreply.github.com>
2023-11-19 12:58:52 +0200

    fixed bug in loop

--- miniGame.cpp
+++ miniGame.cpp
@@ -19,54 +19,60 @@ int main()
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> distribution(0, 2);
 
-    while (ourPoints != 3 || theirPoints != 3) {
+    while (ourPoints != 3 && theirPoints != 3) {
         //random_number = distribution(gen);
         random_number = 2;
         cout << "Введите ваш выбор(Первая буква большая, названия только на АНГЛИЙСКОМ Stone, Paper, Scissors): "; cin >> choice;
         if (choice != "Stone" && choice != "Scissors" && choice != "Paper") {
-            cout << "Такого хода не существует!" << endl;
+            cout << "Такого хода не существует!" << endl << endl;
             choice.clear();
             continue;
         }
-        cout << "Наш ход: " << material[random_number] << endl;
+        cout << "Наш ход: " << material[random_number] << endl << endl;
 
         if (material[random_number] == choice) {
-            cout << "Наши хода совпали, переигрываем.";
+            cout << "Наши хода совпали, переигрываем." << endl << endl;
             continue;
         }
         else if (material[random_number] == "Stone") {
             if (choice == "Scissors") {
-                cout << "Проигришь((. Камень побеждает ножницы. Наши очки: " << ++ourPoints << ". Ваши очки: " << theirPoints << endl;
+                ourPoints++;
+                cout << "Проигришь((. Камень побеждает ножницы. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
             else {
-                cout << "Победа!). Бумага побеждает камень. Наши очки: " << ourPoints << ". Ваши очки: " << ++theirPoints << endl;
+                theirPoints++;
+                cout << "Победа!). Бумага побеждает камень. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
         }
         else if (material[random_number] == "Scissors") {
             if (choice == "Paper") {
-                cout << "Проигришь((. Ножницы побеждают бумагу. Наши очки: " << ++ourPoints << ". Ваши очки: " << theirPoints << endl;
+                ourPoints++;
+                cout << "Проигришь((. Ножницы побеждают бумагу. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
             else {
-                cout << "Победа!). Камень побеждает ножницы. Наши очки: " << ourPoints << ". Ваши очки: " << ++theirPoints << endl;
+                theirPoints++;
+                cout << "Победа!). Камень побеждает ножницы. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
         }
         else {
             if (choice == "Stone") {
-                cout << "Проигришь((. Бумага побеждает камень. Наши очки: " << ++ourPoints << ". Ваши очки: " << theirPoints << endl;
+                ourPoints++;
+                cout << "Проигришь((. Бумага побеждает камень. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
             else {
-                cout << "Победа!). Ножницы побеждают бумагу. Наши очки: " << ourPoints << ". Ваши очки: " << ++theirPoints << endl;
+                theirPoints++;
+                cout << "Победа!). Ножницы побеждают бумагу. Наши очки: " << ourPoints << ". Ваши очки: " << theirPoints << endl << endl;
             }
         }
         choice.clear();
     }
 
     if (ourPoints > theirPoints) {
-        cout << "К сожалению вы проиграли и по этому все файлы останутся зашифроваными(" << endl;
+        cout << "К сожалению вы проиграли и по этому все файлы останутся зашифроваными(" << endl << endl;
         //TODO Скорее всего просто выходим из программы 
     }
     else {
-        cout << "Поздравляем)). Вы победили, мы здержим свое обещание а расшифруем файлы" << endl;
+        cout << "Поздравляем)). Вы победили, мы здержим свое обещание а расшифруем файлы" << endl << endl;
         //TODO Вызываем функцию расшифровки файлов
     }
 
